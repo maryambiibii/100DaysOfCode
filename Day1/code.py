42409121250769b18feea6ef6f6197c954b9c26d
Maryam Bibi <97449360+maryambiibii@users.noreply.github.com>
2022-01-10 18:16:04 +0500

    Update code.py

--- Day1/code.py
+++ Day1/code.py
@@ -1,1 +1,37 @@
+#Exercises
+
+#Day 1-1: Printing 👇
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+#Day 1-2: Debugging Practice
+#Fix the code below 👇
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+#Day 1-3: Input Function
+print(len(input("What is your name?")))
+
+#Day 1-4: Variables
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+c = a
+a = b
+b = c
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
 
