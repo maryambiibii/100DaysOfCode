d65831c955e472bb1868b893adf1fa982b325825
Maryam Bibi <97449360+maryambiibii@users.noreply.github.com>
2022-01-15 19:19:08 +0500

    Update Project_Code.py

--- Day6/Project_Code.py
+++ Day6/Project_Code.py
@@ -1,1 +1,16 @@
-
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+while not at_goal():
+    if not wall_on_right():
+        turn_right()
+        move()    
+        if wall_on_right() and not front_is_clear():
+            turn_left()  
+    elif front_is_clear():
+        move()
+    elif wall_on_right() and not front_is_clear():
+        turn_left()
+       
