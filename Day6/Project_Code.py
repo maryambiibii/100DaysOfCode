f3af1488bc43feedaa3b3af6ea147a6bf4f0147a
Maryam Bibi <97449360+maryambiibii@users.noreply.github.com>
2022-01-15 19:25:23 +0500

    Update Project_Code.py

--- Day6/Project_Code.py
+++ Day6/Project_Code.py
@@ -15,5 +15,5 @@ while not at_goal():
         turn_left()
        
 
-![This is an image](https://github.com/maryambiibii/100DaysOfCode/blob/main/Day6/Img/Screen%20Shot%202022-01-15%20at%207.18.30%20PM.png)
+
 
